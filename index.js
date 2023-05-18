c2330df169509116a624a1a3c037eb45b388e87c
japsieeee <dyollsopaj@gmail.com>
2023-05-18 14:51:49 +0800

    [fix] app set relative to dirname

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 const app = express();
 const cookieParser = require("cookie-parser");
 const mongoConnection = require("./connection");
 
+app.use(express.static(path.join(__dirname, "./public")));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
